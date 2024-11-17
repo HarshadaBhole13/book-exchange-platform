ef206a5f2af03eacfdc91fd21948514870d95c3a
Harshada Bhole <harshada@Harshadas-MacBook-Air.local>
2024-11-17 13:26:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React from 'react';
 import './App.css';
+import Register from './components/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router, Route, and Switch
+import HomePage from './components/Home';
+import Profile from './components/Profile'
+import Dashboard from './components/dashboard.js'
+import UserDashboard from './components/UserDashboard.js';
+import AddBook from './components/AddBook.js';
+import UpdateBook from './components/UpdateBook.js';
+import UserBooks from './components/UserBooks.js';
+import Headers from './components/Header.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        
+      <Router>
+            <Routes> {/* Use Routes instead of Switch */}
+                <Route path="/" element={<HomePage />} />
+                <Route path="/register" element={<Register />} />
+                {/* Add routes for Login and Profile pages here */}
+                <Route path="/dashboard" element={<Dashboard />}/>
+                <Route path="/profile" element={<Profile />}/>
+                <Route path='/UserDashboard' element={<UserDashboard/>}/>
+                <Route path='/AddBook' element={<AddBook/>}/>
+                <Route path='/UpdateBook' element={<UpdateBook/>}/>
+                <Route path='/user-books' element={<UserBooks/>}/>
+                <Route path='/Header' element={<Headers/>}/>
+            </Routes>
+        </Router>
+        
+    );
 }
 
 export default App;
