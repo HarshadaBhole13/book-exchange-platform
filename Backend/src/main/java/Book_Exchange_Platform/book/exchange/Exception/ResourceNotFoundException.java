ff0bf6b5cdc08a27dd7bb5143be528be1104c4ed
HarshadaBhole13 <harshadabhole4@gmail.com>
2024-11-17 17:15:54 +0530

    Add files via upload

---
+++ Backend/src/main/java/Book_Exchange_Platform/book/exchange/Exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package Book_Exchange_Platform.book.exchange.Exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
